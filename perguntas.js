facb9b538d5586672edeb40c6e9dae3b7d9e399a
AlejandroHenrique17 <00001113237788sp@al.educacao.sp.gov.br>
2024-11-27 14:10:38 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'RPG',
+    'O que é RPG?',
+    'RPG (Role Play Game) é um modo de jogo na onde você interpreta seu personagem.'
+)
+
+criaCartao(
+    'Marketing',
+    'Qual a nossa forma de comunicação?',
+    'A forma de comunicação seria por redes sociais, como por exemplo: TikTok, YouTube, Twitter, Instagram.'
+)
+
+criaCartao(
+    'Produto',
+    'Qual o nosso produto?',
+    'Jogos de RPG, e Merch.'
+)
+
+criaCartao(
+    'Concorrencia',
+    'Qual os nossos concorrentes?',
+    'Nintendo, Hoyoverse, Ubisoft, e etc.'
+)
